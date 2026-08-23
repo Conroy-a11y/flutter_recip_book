3d6100618430d46748c6a1ecc2857b8ebc2e7e96
Conroy Makhubu <cmakhubu@gmail.com>
2026-08-23 18:00:37 +0200

    Update login_page.dart
    add: Text widget and SizeBox widget on the column

--- recipe_book/recipe_book/lib/pages/login_page.dart
+++ recipe_book/recipe_book/lib/pages/login_page.dart
@@ -13,6 +13,21 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.green,
       appBar: AppBar(title: Text('Login'), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'RecipBook',
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.w300),
+            ),
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width * 0.90,
+              height: MediaQuery.sizeOf(context).height * 0.30,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
